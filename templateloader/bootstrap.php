df6ac2ab294324390b2a17dd4819b935a430dc9b
Nikos Tsaganos <tsag99@gmail.com>
2015-05-11 16:54:55 +0300

    Config bug fix

--- templateloader/bootstrap.php
+++ templateloader/bootstrap.php
@@ -3,10 +3,11 @@
 mb_internal_encoding("UTF-8");
 error_reporting(E_ALL ^ E_NOTICE);
 
+define('TEMPLATELOADER_ROOT', dirname(__FILE__)); 
 define('ROOT', dirname(dirname(__FILE__))); 
 
-require_once('TemplateLoader.php');
-$config = require_once('config.php');
+require_once(TEMPLATELOADER_ROOT.'/TemplateLoader.php');
+$config = include(TEMPLATELOADER_ROOT.'/config.php');
 
 $templateLoader = new TemplateLoader($config);
 $templateLoader->run();
